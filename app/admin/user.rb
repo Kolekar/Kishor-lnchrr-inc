57f639571d55548746c27a852358278a65caa8a8
Author Name <email@address.com>
2014-08-18 04:25:08 +0530

    integrated themes bug pending

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,7 +1,9 @@
 ActiveAdmin.register User do
   permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip
 
-  
+  controller do
+    layout 'active_admin'
+  end
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
